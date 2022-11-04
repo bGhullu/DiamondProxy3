373795468b67f547af10a81f1afb985f9cc1d3f7
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2022-11-04 16:46:03 +0000

    ERC2535

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-
+const MAINNET_RPC_URL= process.env.MAINNET_RPC_URL
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
@@ -15,10 +15,14 @@ module.exports = {
   solidity: "0.8.17",
   defaultNetwork: "hardhat",
   networks: {
-      hardhat: {
-          chainId: 31337,
-          blockConfirmations: 1,
+    hardhat: {
+      chainId: 31337,
+      forking: {
+          url: MAINNET_RPC_URL,
+          blockNumber:15796924,
+          allowUnlimitedContractSize: true,
       },
+  },
       goerli: {
           chainId: 5,
           blockConfirmations: 6,
